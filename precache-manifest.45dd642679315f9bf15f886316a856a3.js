2c96f3fe921f45de6485cae54c100ab24816163d
Shaz Momin <shazmomin@MacBook-Pro.local>
2020-07-28 15:28:43 -0500

    Updates

---
+++ precache-manifest.45dd642679315f9bf15f886316a856a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1532f481922e70b419914f4053482386",
+    "url": "/Weather-Widgets/index.html"
+  },
+  {
+    "revision": "9fbaf49259fb9c49669b",
+    "url": "/Weather-Widgets/static/css/main.19e1a092.chunk.css"
+  },
+  {
+    "revision": "c95397693893e6d5de41",
+    "url": "/Weather-Widgets/static/js/2.cb627703.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Weather-Widgets/static/js/2.cb627703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fbaf49259fb9c49669b",
+    "url": "/Weather-Widgets/static/js/main.c6d436ca.chunk.js"
+  },
+  {
+    "revision": "5a00d68381c5c6213a7e",
+    "url": "/Weather-Widgets/static/js/runtime-main.6c5eab39.js"
+  },
+  {
+    "revision": "1aacd0d320538f8c2dfff4c1f2ed774c",
+    "url": "/Weather-Widgets/static/media/sky_1920.1aacd0d3.jpg"
+  }
+]);
